082d7c79f8e677a330a376526864c0354151f2b8
Zaki Rehnoom <zakirehnoom9402@gmail.com>
2026-06-02 19:20:59 +0600

    check

--- audio-watermark/src/main/java/com/convo/audio_watermark/controller/WatermarkConfigController.java
+++ audio-watermark/src/main/java/com/convo/audio_watermark/controller/WatermarkConfigController.java
@@ -14,16 +14,16 @@ public class WatermarkConfigController {
     @Autowired
     private WatermarkConfigService service;
 
-    @GetMapping("/config")
-    public Map<String, Object> getConfig(
-            @RequestParam String roomId,
-            @RequestParam String userId) {
-        WatermarkConfig config = service.getOrCreateConfig(roomId, userId);
-        System.out.println("Returning watermark config for roomId=" + roomId + " userId=" + userId);
+    // @GetMapping("/config")
+    // public Map<String, Object> getConfig(
+    //         @RequestParam String roomId,
+    //         @RequestParam String userId) {
+    //     WatermarkConfig config = service.getOrCreateConfig(roomId, userId);
+    //     System.out.println("Returning watermark config for roomId=" + roomId + " userId=" + userId);
 
-        return Map.of(
-                "seed", config.getSeed(),
-                "alpha", config.getAlpha(),
-                "frameSize", config.getFrameSize());
-    }
+    //     return Map.of(
+    //             "seed", config.getSeed(),
+    //             "alpha", config.getAlpha(),
+    //             "frameSize", config.getFrameSize());
+    // }
 }
